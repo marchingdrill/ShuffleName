9a041a2b3bbfebbd537473a42ee69caee2c539fa
mk <matindoril@gmail.com>
2024-08-30 01:23:49 +0900

    シャッフル機能を実装

---
+++ BlazorWasm1/ShuffleWordService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorWasm1
+{
+    public class ShuffleWordService
+    {
+        // 文字列配列の順序をランダムに入れ替える
+        public string[] Shuffle(IEnumerable<string> words)
+        {
+            var random = new Random();
+            return words.OrderBy(_ => random.Next()).ToArray();
+        }
+    }
+}
